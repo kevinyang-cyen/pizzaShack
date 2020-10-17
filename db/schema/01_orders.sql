f37e92948e3cd777120f2ddc668091be4aae43a9
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-17 20:17:15 +0000

    added orders, pizzas, pizzas_orders(bridge) table

---
+++ db/schema/01_orders.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS pizzas CASCADE;
+DROP TABLE IF EXISTS pizzas_orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(10) NOT NULL,
+  received_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  order_status VARCHAR(50) SET DEFAULT 'order incomplete'
+);
+
+CREATE TABLE pizzas (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(255) NOT NULL,
+price decimal(3,2),
+ingredients TEXT,
+image url set DEFAULT 'http://placekitten.com/125/125'
+);
+
+CREATE TABLE pizzas_orders(
+  id SERIAL PRIMARY KEY NOT NULL,
+  pizza_id REFERENCING pizzas.id CASCADE ON DELETE,
+  order_id REFERENCING orders.id CASCADE ON DELETE
+);
