17f68befeba3fe124f93214187ed2dc20530dc7c
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-17 22:10:31 +0000

    updating schema

--- db/schema/01_orders.sql
+++ db/schema/01_orders.sql
@@ -5,9 +5,9 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   phone_number VARCHAR(12) NOT NULL,
-  received_at TIMESTAMP,
+  received_at TIMESTAMP DEFAULT now(),
   completed_at TIMESTAMP,
-  order_status VARCHAR(50) SET DEFAULT 'order incomplete'
+  order_status VARCHAR(50) SET DEFAULT 'pending'
 );
 
 CREATE TABLE pizzas (
@@ -21,5 +21,6 @@ image url set DEFAULT 'http://placekitten.com/125/125'
 CREATE TABLE pizzas_orders(
   id SERIAL PRIMARY KEY NOT NULL,
   pizza_id REFERENCING pizzas.id CASCADE ON DELETE,
-  order_id REFERENCING orders.id CASCADE ON DELETE
+  order_id REFERENCING orders.id CASCADE ON DELETE,
+  quantity INT NOT NULL
 );
--- db/seeds/01_orders.sql
+++ db/seeds/01_orders.sql
@@ -1,12 +1,15 @@
 -- Users table seeds here (Example)
-INSERT INTO orders (name, phone_number)
-VALUES ('Alice', '+16502414473'),
+INSERT INTO orders (name, phone_number, received_at, completed_at, order_status)
+VALUES ('Alice Wonderland', '+16502414473', '2018-02-12T08:13:20.000Z', '2018-02-12T08:13:20.000Z', ),
 ('Lebron James', '+16502414473'),
-('Ash Ketchum', '+16502414473')
+('Ash Ketchum', '+16502414473');
 
 INSERT INTO pizzas (name, price, ingredients) VALUES
 ('Pepperoni', 17.38, 'Handmade bread dough, Marinara Sauce, Four different types of artisanal cheese, hand-sliced pepperoni, garlic butter brushed crust'),
-('Meat Lovers', 17.76, 'Hand-delivered 100% Kobe Waygu Beef sliced, Capicola Sausage from Vienna, Genovese Pepperoni, Marinated Jerk Chicken, our famous four-artisanal cheese blend, marinara sauce and garlic brushed ')
-('')
+('Meat Lovers', 17.76, 'Hand-delivered 100% Kobe Waygu Beef sliced, Capicola Sausage from Vienna, Genovese Pepperoni, Marinated Jerk Chicken, our famous four-artisanal cheese blend, marinara sauce and garlic brushed '),
+('The Terminator', 10.00, 'Ground Beef, Austrian Sausage, American Cheddar Cheese, and BBQ Sauce, this slice is out of this world');
 
-INSERT INTO pizzas_orders()
+INSERT INTO pizzas_orders(pizza_id, order_id, quantity)
+VALUES (1, 2, 3),
+(2, 2, 1)
+(3, 1, 1);
