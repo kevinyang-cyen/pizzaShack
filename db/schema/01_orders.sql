f5bc60f52465fd04b88deef83cca045824e5c996
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-17 22:24:45 +0000

    all table tested and ready to go

--- db/schema/01_orders.sql
+++ db/schema/01_orders.sql
@@ -15,30 +15,13 @@ id SERIAL PRIMARY KEY NOT NULL,
 name VARCHAR(255) NOT NULL,
 price decimal(3,2),
 ingredients TEXT,
-image url DEFAULT 'http://placekitten.com/125/125'
+image_url TEXT DEFAULT 'http://placekitten.com/125/125'
 );
 
 CREATE TABLE pizzas_orders(
   id SERIAL PRIMARY KEY NOT NULL,
-  pizza_id REFERENCES pizzas(id) CASCADE ON DELETE,
-  order_id REFERENCES orders(id) CASCADE ON DELETE,
+  pizza_id INTEGER REFERENCES pizzas(id) ON DELETE CASCADE,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
   quantity INT NOT NULL
 );
 
-
-
-psql:db/schema/01_orders.sql:1: NOTICE:  table "orders" does not exist, skipping
-DROP TABLE
-psql:db/schema/01_orders.sql:2: NOTICE:  table "pizzas" does not exist, skipping
-DROP TABLE
-psql:db/schema/01_orders.sql:3: NOTICE:  table "pizzas_orders" does not exist, skipping
-DROP TABLE
-psql:db/schema/01_orders.sql:11: ERROR:  syntax error at or near "SET"
-LINE 7:   order_status VARCHAR(50) SET DEFAULT 'pending'
-                                   ^
-psql:db/schema/01_orders.sql:19: ERROR:  syntax error at or near "set"
-LINE 6: image url set DEFAULT 'http://placekitten.com/125/125'
-                  ^
-psql:db/schema/01_orders.sql:26: ERROR:  syntax error at or near "pizzas"
-LINE 3:   pizza_id REFERENCING pizzas.id CASCADE ON DELETE,
-                               ^
