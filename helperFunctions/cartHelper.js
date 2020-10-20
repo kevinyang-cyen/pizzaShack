f5dc6ae3d84ae88715cac9c64027baa81ee6c9ae
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-20 23:56:27 +0000

    working on adding the quotes

--- helperFunctions/cartHelper.js
+++ helperFunctions/cartHelper.js
@@ -7,11 +7,18 @@
 const cartHelper = (cart) => {
   let keys = Object.keys(cart);
   let pizzaInCart = [];
+  let left = ';
+  let right = ';
+
   for (key of keys) {
     if (cart[key] > 0) {
+      let key1 = left + key + right
+      key1.toString
+      console.log(key1,"key1")
       pizzaInCart.push(key);
     }
   };
+
   return pizzaInCart
 }
 module.exports = cartHelper;
