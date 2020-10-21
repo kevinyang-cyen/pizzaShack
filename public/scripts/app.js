6a5bd1cfcf26d6f7afbbee7c7ae04b68faa82e2c
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-21 02:52:19 +0000

    changing so it sores in local object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,22 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$(document).ready(() => {
+  function getCart() {
+    const sessionCart = sessionStorage.getItem('cart');
+    return sessionCart || {};
+  }
+
+  function setCart(itemName, quantity) {
+    /**
+     * {
+        'Pepperoni': 1,
+        'The Terminator': 1,
+        'Meat Lovers': 2
+      };
+     */
+    // update client-side object
+    window.userCart[itemName] = quantity;
+    // update session storage to persist cart
+    sessionStorage.setItem('cart', userCart);
+  }
+
+  window.userCart = getCart();
 });
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -15,27 +15,34 @@ module.exports = (db) => {
    * 2. We want this route to only update data and send back the "new cart"
    */
   router.post("/", (req, res) => {
-    console.log(req.params)
-    cart[id]= req.params.quantity;
-    // re renders page after cart item updated
-    let pizzaInCart = cartHelper(cart);
+    const formData = req.body;
+    const quantity = formData.quantity;
+    const pizzaName = formData.pizzaName;
+
+    // pizzaName is our key for the cart
+    cart[pizzaName] = quantity;
+    res.json(cart);
+    // console.log(req.params)
+    // cart[id]= req.params.quantity;
+    // // re renders page after cart item updated
+    // let pizzaInCart = cartHelper(cart);
 
 
-    console.log(pizzaInCart, 'PIZZA in CART');
-    const query = `SELECT * from pizzas WHERE name = ANY(array[${pizzaInCart}]);`
-    console.log(query, "query");
-    db.query(query)
-      .then(data => {
-        const pizzas = data.rows;
-        const templateVars = { pizzas, cart };
-        console.log(templateVars, 'template vars');
-        res.render("order_page_template", templateVars);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    // console.log(pizzaInCart, 'PIZZA in CART');
+    // const query = `SELECT * from pizzas WHERE name = ANY(array[${pizzaInCart}]);`
+    // console.log(query, "query");
+    // db.query(query)
+    //   .then(data => {
+    //     const pizzas = data.rows;
+    //     const templateVars = { pizzas, cart };
+    //     console.log(templateVars, 'template vars');
+    //     res.render("order_page_template", templateVars);
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
   });
 
 
