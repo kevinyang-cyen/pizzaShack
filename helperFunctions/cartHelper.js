c0cf6e94142245dfcc25819d8e20d0cbab375582
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-22 19:36:32 +0000

    debugging

--- helperFunctions/cartHelper.js
+++ helperFunctions/cartHelper.js
@@ -8,7 +8,7 @@ const cartHelper = (cart) => {
   let keys = Object.keys(cart);
   let pizzaInCart = [];
 
-  for (key of keys) {
+  for (let key of keys) {
     if (cart[key] > 0) {
       pizzaInCart.push(key);
     }
